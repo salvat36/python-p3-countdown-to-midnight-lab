febd6c5411fba8e25129f83b71cba4ab58cabec6
Ryan <salvat36@gmail.com>
2023-05-22 14:56:43 -0500

    passing labs

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import time
+
+def countdown(i):
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(i):
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+        time.sleep(1)
+    print('HAPPY NEW YEAR!')
